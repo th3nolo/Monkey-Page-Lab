554c06d89caad3d2a13543ed69d4eba9a3b01e9b
Paola Aguilar <paola.aguilar@goluuk.com>
2022-05-22 17:55:58 -0600

    footer labels

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -144,12 +144,12 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-blueGray-500 font-semibold py-1">
-                Copyright © {new Date().getFullYear()} Notus NextJS by{" "}
+                © {new Date().getFullYear()} Monkey lab page by{" "}
                 <a
                   href="https://www.creative-tim.com?ref=nnjs-footer"
                   className="text-blueGray-500 hover:text-blueGray-800"
                 >
-                  Creative Tim
+                  Zingiber
                 </a>
                 .
               </div>
