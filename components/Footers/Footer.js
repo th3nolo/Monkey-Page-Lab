668aeb98b360e8b069d7fd7e332036bfdc420f02
Paola Aguilar <paola.aguilar@goluuk.com>
2022-05-27 21:01:01 -0600

    footer label

--- components/Footers/Footer.js
+++ components/Footers/Footer.js
@@ -29,13 +29,11 @@ export default function Footer() {
           <div className="flex flex-wrap items-center md:justify-between justify-center">
             <div className="w-full md:w-4/12 px-4 mx-auto text-center">
               <div className="text-sm text-blueGray-500 font-semibold py-1">
-                © {new Date().getFullYear()} Monkey lab page by{" "}
+                © {new Date().getFullYear()} Monkey Page Lab
                 <a
                   href="https://www.creative-tim.com?ref=nnjs-footer"
                   className="text-blueGray-500 hover:text-blueGray-800"
-                >
-                  Zingiber
-                </a>
+                ></a>
                 .
               </div>
             </div>
